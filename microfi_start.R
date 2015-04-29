dfb22a72c34f089a9794cba0ec88ae3e9273a13d
Tom Besio <thomas.besio@gmail.com>
2015-04-29 10:53:31 -0500

    Added Q1 and Q2 to the script

--- microfi_start.R
+++ microfi_start.R
@@ -1,4 +1,4 @@
-## microfinance network 
+## microfinance network
 ## data from BANERJEE, CHANDRASEKHAR, DUFLO, JACKSON 2012
 
 ## data on 8622 households
@@ -17,7 +17,7 @@ edges <- read.table("microfi_edges.txt", colClasses="character")
 hhnet <- graph.edgelist(as.matrix(edges))
 hhnet <- as.undirected(hhnet) # two-way connections.
 
-## igraph is all about plotting.  
+## igraph is all about plotting.
 V(hhnet) ## our 8000+ household vertices
 ## Each vertex (node) has some attributes, and we can add more.
 V(hhnet)$village <- as.character(hh[V(hhnet),'village'])
@@ -33,7 +33,7 @@ V(hhnet)$label=NA
 # The graphs can take a very long time, but I've found
 # edge.curved=FALSE speeds things up a lot.  Not sure why.
 
-## we'll use induced.subgraph and plot a couple villages 
+## we'll use induced.subgraph and plot a couple villages
 village1 <- induced.subgraph(hhnet, v=which(V(hhnet)$village=="1"))
 village33 <- induced.subgraph(hhnet, v=which(V(hhnet)$village=="33"))
 
@@ -48,7 +48,7 @@ library(gamlr)
 ## match id's; I call these 'zebras' because they are like crosswalks
 zebra <- match(rownames(hh), V(hhnet)$name)
 
-## calculate the `degree' of each hh: 
+## calculate the `degree' of each hh:
 ##  number of commerce/friend/family connections
 degree <- degree(hhnet)[zebra]
 names(degree) <- rownames(hh)
@@ -57,5 +57,50 @@ degree[is.na(degree)] <- 0 # unconnected houses, not in our graph
 ## if you run a full glm, it takes forever and is an overfit mess
 # > summary(full <- glm(loan ~ degree + .^2, data=hh, family="binomial"))
 # Warning messages:
-# 1: glm.fit: algorithm did not converge 
-# 2: glm.fit: fitted probabilities numerically 0 or 1 occurred 
+# 1: glm.fit: algorithm did not converge
+# 2: glm.fit: fitted probabilities numerically 0 or 1 occurred
+
+## Q1 - Log Transformation of degree
+# We add the '+1' term so that the log is defined when degree = 0
+d <- log(degree+1)
+
+## Q2 - Use a Lasso Regression to fit dhat based on x
+# Create a matrix to hold the controls
+# First need to convert beds, rooms, and leader to factors
+hh$rooms <- factor(hh$rooms)
+hh$beds <- factor(hh$beds)
+hh$leader <- factor(hh$leader)
+
+# Now put all of the controls in a single data frame
+controls <- data.frame(hh[,c(2:9)])
+
+# And re level everything to add NA
+controls$village <- factor(controls$village,levels=c(NA,levels(controls$village)), exclude=NULL)
+controls$religion <- factor(controls$religion,levels=c(NA,levels(controls$religion)), exclude=NULL)
+controls$roof <- factor(controls$roof,levels=c(NA,levels(controls$roof)), exclude=NULL)
+controls$beds <- factor(controls$beds,levels=c(NA,levels(controls$beds)), exclude=NULL)
+controls$electricity <- factor(controls$electricity,levels=c(NA,levels(controls$electricity)), exclude=NULL)
+controls$ownership <- factor(controls$ownership,levels=c(NA,levels(controls$ownership)), exclude=NULL)
+controls$leader <- factor(controls$leader,levels=c(NA,levels(controls$leader)), exclude=NULL)
+
+# Convert the data frame into a sparse model matrix
+controls.smm <- sparse.model.matrix(~., data=controls)[,-1]
+
+# And the y variable into a separate vector
+y <- hh[,1]
+
+# Now we can fit the controls to d
+treat <- gamlr(controls.smm,d)
+
+# Predict dhat from the fitted betas
+dhat <- predict(treat, controls.smm, type="response")
+
+# And plot to see the relationship between d and dhat
+plot(dhat,d,bty="n",pch=21,bg=8)
+
+# There does not graphically appear to be a strong relationship. But check R^2
+cor(drop(dhat),d)^2
+# ~> [1] 0.08478302
+# R^2 is low which is good for our sake as the controls do not help predict the treatment effect
+
+
